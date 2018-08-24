30965c59d9d9b314603ce02f634e6f68f939a0e4
PzalDigital <pzaljr@gmail.com>
2018-08-24 12:32:14 -0700

    Powershell Stub
    I've decided to start keeping track of PowerShell things in this repo as well.

---
+++ examples/powershell-comments.ps1
@@ -0,0 +1,22 @@
+<#
+.SYNOPSIS
+    A brief description of the function or script. This keyword can be used
+    only once in each topic.
+.DESCRIPTION
+    A detailed description of the function or script. This keyword can be
+    used only once in each topic.
+.NOTES
+    File Name      : xxxx.ps1
+    Author         : J.P. Blanc (jean-paul_blanc@silogix-fr.com)
+    Prerequisite   : PowerShell V2 over Vista and upper.
+    Copyright 2011 - Jean Paul Blanc/Silogix
+.LINK
+    Script posted over:
+    http://silogix.fr
+.EXAMPLE
+    Example 1
+.EXAMPLE
+    Example 2
+#>
+Function blabla
+{}
---
+++ powershell.md
@@ -0,0 +1,15 @@
+## PowerShell 
+
+# Know your Powershell Version:
+`$PSVersionTable.PSVersion`
+@see https://stackoverflow.com/a/1825807
+
+## chocolatey: Windows Package Manager
+@see https://chocolatey.org/install
+- Version Command: `choco -v`
+
+# Commands Reference
+https://github.com/chocolatey/choco/wiki/CommandsList
+
+## Commenting in .PS1 files
+see: `examples/powershell-comments.ps1`
